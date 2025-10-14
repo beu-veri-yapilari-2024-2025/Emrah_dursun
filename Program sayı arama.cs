967ac609186fc0e2ac8242b385942f23977a7026
Emrah Dursun <emrahdursun935@gmail.com>
2025-10-14 18:55:25 +0300

    Add files via upload

---
+++ Program sayı arama.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class SearchInArray
+{
+    static void Main()
+    {
+        int[] numbers = { 3, 7, 2, 9, 4 };
+        Console.Write("Aranacak sayıyı girin: ");
+        int search = int.Parse(Console.ReadLine());
+
+        bool found = false;
+
+        foreach (int number in numbers)
+        {
+            if (number == search)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (found)
+            Console.WriteLine("Sayı dizide bulundu.");
+        else
+            Console.WriteLine("Sayı dizide bulunamadı.");
+    }
+}
